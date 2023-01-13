48dcaa2140a26c7dc70f180e59153fa693d4e2d7
ca r214.08 <ca.r214.08@ca.local>
2023-01-13 19:50:32 +0400

    products app

---
+++ client/src/pages/products/index.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect } from "react";
+import "./index.scss";
+import { Helmet } from "react-helmet";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteData, fetchData } from "../../redux/slice/getData";
+import { Space, Spin } from "antd";
+import SearchComp from "../../components/search";
+import { Link } from "react-router-dom";
+import { Button } from "antd";
+
+const Products = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
+  useEffect(() => {
+    dispatch(fetchData(""));
+  }, []);
+
+  const handleSortByPrice = () => {
+    const test = products?.data?.sort((a, b) => a?.price - b?.price);
+    console.log(test);
+  };
+
+  return (
+    <div id="products-page">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Products</title>
+      </Helmet>
+      <div className="container">
+        <div className="search-sort">
+          <SearchComp />
+          <div className="sort">
+            <Button onClick={() => handleSortByPrice()} type="primary" ghost>
+              Sort By Price
+            </Button>
+          </div>
+        </div>
+        <div className="products">
+          {products.loading ? (
+            <div className="spin">
+              <Space size="middle">
+                <Spin size="small" />
+                <Spin />
+                <Spin size="large" />
+              </Space>
+            </div>
+          ) : (
+            products?.data?.map((element) => {
+              return (
+                <div key={element.id} className="card">
+                  <Link to={`/${element.id}`}>
+                    <div className="img">
+                      <img src={element.imgUrl} alt="" />
+                    </div>
+                  </Link>
+                  <div className="about">
+                    <h2>{element.name}</h2>
+                    <div className="title">
+                      <p className="id">
+                        <span className="name">Product Id:</span>{" "}
+                        <span className="value">{element.id}</span>
+                      </p>
+                      <p className="price">
+                        <span className="name">Price:</span>{" "}
+                        <span className="value">{element.price}</span>
+                      </p>
+                      <p className="description">
+                        <span className="name">Description:</span>{" "}
+                        <span className="value">{element.description}</span>
+                      </p>
+                    </div>
+                    <div className="btn">
+                      <Button
+                        onClick={(e) => {
+                          dispatch(deleteData(element.id)).then((el) =>
+                            dispatch(fetchData(""))
+                          );
+                        }}
+                        type="primary"
+                        danger
+                      >
+                        Delete
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          )}
+          {products.loading
+            ? null
+            : products.error && (
+                <div
+                  style={{
+                    fontSize: "20px",
+                    fontWeight: "700",
+                    color: "red",
+                    marginTop: "20px",
+                  }}
+                >
+                  {products.error}
+                </div>
+              )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
---
+++ server/server.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 8000;
+let idCount = 5;
+let products = [
+  {
+    id: 55,
+    name: "Kylynn Brock",
+    price: 2,
+    description: "Eos aute voluptatem voluptatem voluptatem aute ",
+    imgUrl:
+      "https://dlcdnwebimgs.asus.com/gain/aad81170-e69a-4054-9db1-006a85588c9d/",
+  },
+  {
+    id: 1,
+    name: "Kylynn Brock",
+    price: 15,
+    description: "Eos aute voluptatem voluptatem voluptatem aute ",
+    imgUrl:
+      "https://dlcdnwebimgs.asus.com/gain/d895a2b9-233a-4f11-91fd-b22788ac973f/",
+  },
+  {
+    id: 2,
+    name: "Kylynn Brock",
+    price: 3,
+    description: "Eos aute voluptatem voluptatem voluptatem aute ",
+    imgUrl:
+      "https://dlcdnwebimgs.asus.com/gain/7449fc82-c00d-474e-8eed-89e2586d5b2e/",
+  },
+  {
+    id: 3,
+    name: "Kylynn Brock",
+    price: 28,
+    description: "Eos aute voluptatem voluptatem voluptatem aute ",
+    imgUrl:
+      "https://www.asus.com/media/global/gallery/vat73rr6tsw3q8dz_setting_xxx_0_90_end_2000.png",
+  },
+  {
+    id: 4,
+    name: "Kylynn Brock",
+    price: 233,
+    description: "Eos aute voluptatem voluptatem voluptatem aute ",
+    imgUrl:
+      "https://www.asus.com/media/global/gallery/vat73rr6tsw3q8dz_setting_xxx_0_90_end_2000.png",
+  },
+];
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/products", (req, res) => {
+  res.send(products);
+});
+
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const selectProduct = products.find((product) => product.id == id);
+  if (selectProduct) {
+    res.send(selectProduct);
+  } else {
+    res.status(404).json({ message: "there is no such user..." });
+  }
+});
+
+app.delete("/products/:id", (req, res) => {
+  const { id } = req.params;
+  products = products.filter((element) => element.id != id);
+  res.status(200).json({ message: "deleted product" });
+});
+
+app.post("/products", (req, res) => {
+  const productsObj = {
+    id: idCount++,
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+    imgUrl: req.body.imgUrl,
+  };
+  products.push(productsObj);
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}/products`);
+});
